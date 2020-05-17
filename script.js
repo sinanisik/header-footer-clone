e5ea59706cc4a51df442c69698eec084bf230012
Sinan Işık <sinan.isik@ug.bilkent.edu.tr>
2020-05-17 12:14:47 +0300

    edit searchbox

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
 $(document).ready(function() {
-    $('#textSearch_ara').click(function() {
-        var str = $("#search_word").val();
-        alert("Girilen:" + str);
-    });
 
-    $(".textSearch_input").each(function() {
-
-        var $x = $(this).find(".search-icon"),
-            $inp = $(this).find("input:text"),
-            $cle = $(this).find(".reset-icon");
-
-        $inp.click(function() {
-            //            $inp.addClass();
-        });
-
-        $inp.on("input", function(){
-            $cle.toggle(!!this.value);
-        });
-
-        $cle.on("touchstart click", function(e) {
-            e.preventDefault();
-            $inp.val("").trigger("input");
-        });
-
-    });
-
-    $('.textSearch_input').click(function() {
-
-        $(this).css('border', 'none');
-        $('.textSearch_ara').addClass('btn-focused');
+    $('.textSearch_input').click(function(e) {
         $('.search-container').addClass('search-container-focused');
-        $('.search-panel').css('display', 'block');
-
+        $('.textSearch_input').addClass('textSearch_input_focused');
+        $('.textSearch_ara').addClass('btn-focused');
+        $('.search-cont').addClass('.search-panel');
+        e.stopPropagation()
+    });
+    
+    $(document).click(function(){
+        $('.search-container').removeClass('search-container-focused');
+        $('.textSearch_input').removeClass('textSearch_input_focused');
+        $('.textSearch_ara').removeClass('btn-focused');
+        
     });
 
-
-
-
-    //    $('#search_word').focusin(function() {
-    //       $('#textSearch_input').css("border", "none");
-    //    });
 
 });
 
